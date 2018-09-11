cbf464df481638c24af8d2e9456d21fada6be563
571120205@qq.com <571120205@qq.com>
2018-09-12 00:02:54 +0800

    spring-boot-server初始版本

---
+++ spring-boot-sever/src/main/java/com/musingtec/SpringCloudNetfixApplication.java
@@ -0,0 +1,15 @@
+package com.musingtec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//注册Eureka服务的注解
+@EnableEurekaServer
+public class SpringCloudNetfixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudNetfixApplication.class, args);
+	}
+}
